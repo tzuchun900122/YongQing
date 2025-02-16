80b1b8b2889fca2d41a0184b8b6eaf2a6e5cb2d7
tzuchun900122 <tzuchun900122@gmail.com>
2025-02-16 21:03:24 +0800

    連結資料庫

---
+++ YongQing/Models/NorthwindDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace YongQing.Models
+{
+    public class NorthwindDbContext : DbContext
+    {
+        //初始化 NorthwindDbContext，並載入 DbContext 設定
+        public NorthwindDbContext(DbContextOptions<NorthwindDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
--- YongQing/Program.cs
+++ YongQing/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using YongQing.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// 設定 DbContext 並使用 SQL Server
+builder.Services.AddDbContext<NorthwindDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NorthwindConnection")));
 
 var app = builder.Build();
 
